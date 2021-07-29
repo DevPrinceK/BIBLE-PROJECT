05bfcec2c86644367ead582fd0b3bbecdef7e716
BraNanaK <princesamuelpks@gmail.com>
2021-07-29 11:54:07 +0000

    debugging

--- control-files/index.js
+++ control-files/index.js
@@ -1,21 +1,71 @@
 async function start() {
-    let kjvJunk = await fetch('https://raw.githubusercontent.com/thiagobodruk/bible/master/json/en_kjv.json');
+    // fetch books
+    let kjvJunk = await fetch('https://www.abibliadigital.com.br/api/books');
 
-
-
+    // converts books to json
     let kjv = await kjvJunk.json();
 
-    greet(kjv, kjvJunk);
+    // display entire kjv
+    display(kjv);
+
+    // calls the function that get names of the books
+    getNames(kjv);
+
+    // get number of chapters in each
+    getChapters(kjv);
+
 
 }
 
 
+async function getVersions() {
+    let getVersions = await fetch('https://www.abibliadigital.com.br/api/versions');
 
+    let versionJSON = await getVersions.json();
+
+    // get chapters
+    getChapters(versionJSON);
+
+}
+
+
+async function getAllDetails() {
+    let allData = await fetch('https://www.abibliadigital.com.br/api/verses/:version/:gn/:2');
+}
 
 
 function greet(kjv, kjvJunk) {
-    console.log("Hello, your code if running fine. The onpageshow event is firing up the function");
-    console.log(`The KJV is a/an ${typeof (kjv)} datatype`);
-    console.log(`The KJVJUNK is a/an ${typeof (kjvJunk)} datatype`);
-    console.log(kjv);
+    //
+}
+
+// gets all names of books
+function getNames(bibleObj) {
+    let bookNames = Array.from(bibleObj).map(book => book.name);
+
+    //console.log(bookNames);
+    //console.log(typeof bookNames);
+}
+
+// gets all chapters of books  || NOTE can use this to generate the chapters dropdown
+function getChapters(bibleObj) {
+    let chapters = Array.from(bibleObj).map(book => book.chapters)
+    //display(chapters);
+
+    // traversing the chapter
+    traverseChapter(chapters[0]);
+}
+
+
+// iterate through the chapters || NOTE can use this to generate the chapters dropdown
+function traverseChapter(chapter) {
+    for (let i = 0; i < chapter; i++) {
+        console.log(i + 1);
+    }
+}
+
+//
+
+//display function
+function display(item) {
+    console.log(item);
 }
--- control-files/index.mjs
+++ control-files/index.mjs
@@ -1,77 +1,80 @@
-import {
-    kjv
-}
-from '../data-files/en-kjv.mjs';
+    <!-- NOTE -->
+    <!-- <script type="module" src="/control-files/index.mjs"></script> -->
 
-start();
-// Extracting the names || to be used for the dropdown list
+    <!-- NOTE -->
+    <!-- INLINE SCRIPTING -->
+    <!-- INLINE SCRIPTING -->
+    <
+    script type = "module" >
+        // START OF THE INLINE SCRIPTING // NOTE
+        import {
+            kjv
+        }
+    from '../data-files/en-kjv.mjs';
 
-function start() {
-    // this will extract names from the bible
-    let names = kjv.map(books => books.name);
+    start(); // NOTE
+    // // Extracting the names || to be used for the dropdown list
 
-    // // this will extratc chapters from the the selelected book
-    // let chapters = kjv.map(function (books) {
-    //     if (books.name === selectedName) {
-    //         for (let i = 0; i < books.chapters.length; i++) {
-    //             return i + 1;
-    //         }
+    function start() {
+        // this will extract names from the bible
+        let names = kjv.map(books => books.name);
 
-    //     }
-    // })
+        // generates the dropdownlist of books 
+        dropDownNames(names);
 
-    // generates the dropdownlist of books
-    dropDownNames(names);
+        // gets the name of the selected book
+        let bookName = document.getElementById('select-book');
+        let bookNameStr = bookName.options[bookName.selectedIndex].text;
+        console.log(`Selected name is: ${bookNameStr} `);
 
-    // generate the dropdown list of chapters
-    dropDownChapters(chapters);
-}
+        // get chapters and use it to generate dropdown list
+        getChapters(bookNameStr);
 
-// this will get book names
-//console.log(kjv[5].name);
 
-// dropdrown of books
-function dropDownNames(names) {
-    document.getElementById('bible-book').innerHTML = `
-        <select class="form-select" onchange="getChapters(this.value)"> /*TODO function not getting called*/
+        console.log(`Selected name is: ${bookName} `);
+    }
+
+    // this will get book names //console.log(kjv[5].name); // dropdrown of books
+
+
+    // This creates a dropdown list of books
+    function dropDownNames(names) {
+        document.getElementById('bible-book').innerHTML = ` <select
+            class="form-select" id="select-book" /*onchange="getChapters(this.value)"*/> /*TODO function not getting
+            called*/
             <option selected>Choose A Book</option>
             ${names.map(function(name){
             return `<option>${name}</option>`
             }).join("")}
-        </select>
-        `
-}
+            </select>
+            `
+    }
 
-// get selected name and generate dropdown list
-function getChapters(selectedName) {
-    // this will extratc chapters from the the selelected book
-    let chapters = kjv.map(function (books) {
-        if (books.name === selectedName) {
-            for (let i = 0; i < books.chapters.length; i++) {
-                return i + 1;
+    // get selected name and generate dropdown list
+    function getChapters(selectedName) {
+        // this will extratc chapters from the the selelected book
+        let chapters = kjv.map(function (books) {
+            if (books.name === selectedName) {
+                for (let i = 0; i < books.chapters.length; i++) {
+                    return i + 1;
+                }
             }
-        }
-    })
+        })
 
-    // Generates the dropdown list of chapters using the chapters list above
-    dropDownChapters(chapters);
-}
-
-
-// dropdown of chapters
-function dropDownChapters(chapters) {
-    document.getElementById('book-chapter').innerHTML = `
-        <select class="form-select" onchange="getChapters(this.value)">
-            <option selected>Choose A Chapter</option>
-            ${chapters.map(function(chapter){
-            return `<option>${chapter}</option>`
-            }).join("")}
-        </select>
-        `
+        // Generates the dropdown list of chapters using the chapters list above
+        dropDownChapters(chapters);
+    }
 
 
-}
-
-
-// this will get verses
-console.log(kjv[5].chapters[0][0]);
+    // dropdown of chapters 
+    function dropDownChapters(chapters) {
+        document.getElementById('book-chapter').innerHTML = ` <select
+                class="form-select" onchange="getChapters(this.value)">
+                <option selected>Choose A Chapter</option>
+                ${chapters.map(function(chapter){
+                return `<option>${chapter}</option>`
+                }).join("")}
+                </select>
+                `
+    } <
+    /script>
